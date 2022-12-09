af56d988413fe47f3f27a17028bf669223312597
admin <admin@admins-MacBook-Pro.local>
2022-12-09 09:36:10 +0600

    Problem 1

---
+++ bin/first_dart.dart
@@ -0,0 +1,6 @@
+void main() {
+  var a = [1, 7, 12, 3, 56, 2, 87, 34, 54];
+  print(a[0]);
+  print(a[4]);
+  print(a.last);
+}
---
+++ bin/homework.dart
@@ -0,0 +1,8 @@
+void main() {
+  print('Home work 3');
+  int saucer = 2;
+  int spoons = 2;
+  int cups = 2;
+  int result = spoons + saucer + cups;
+  print('Предметов для сервировки стола $result');
+}
